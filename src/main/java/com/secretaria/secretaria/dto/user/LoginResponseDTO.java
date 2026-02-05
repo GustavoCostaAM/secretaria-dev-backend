728e0c01c4f45e468dc2a3466dc58e2d5247f2b1
Kevin de Oliveira <kevin.oliveira@germinare.org.br>
2026-02-05 11:39:44 -0300

    feat: Create login DTOs

---
+++ src/main/java/com/secretaria/secretaria/dto/user/LoginResponseDTO.java
@@ -0,0 +1,11 @@
+package com.secretaria.secretaria.dto.user;
+
+import com.secretaria.secretaria.model.UserRole;
+
+public record LoginResponseDTO(
+        Long id,
+        String name,
+        String username,
+        UserRole role,
+        String token
+) {}
