260ed3ea39e115f59057c93d08536878f1411f04
Kevin de Oliveira <kevin.oliveira@germinare.org.br>
2026-02-19 11:38:07 -0300

    refactor: registerStudent endpoint only register students now

--- src/main/java/com/secretaria/secretaria/controller/UserController.java
+++ src/main/java/com/secretaria/secretaria/controller/UserController.java
@@ -6,6 +6,7 @@ import com.secretaria.secretaria.dto.user.UserUpdateDTO;
 import com.secretaria.secretaria.model.Student;
 import com.secretaria.secretaria.model.Teacher;
 import com.secretaria.secretaria.model.User;
+import com.secretaria.secretaria.model.UserRole;
 import com.secretaria.secretaria.service.user.DeactivateUserService;
 import com.secretaria.secretaria.service.user.RegisterUserService;
 import com.secretaria.secretaria.service.user.UpdateUserService;
@@ -25,6 +26,9 @@ public class UserController {
 
     @PostMapping("/registerStudent")
     public ResponseEntity<UserResponseDTO> registerStudent(@RequestBody @Valid UserRegistrationDTO dto) {
+        if(dto.role() != UserRole.STUDENT){
+            throw new RuntimeException("This endpoint is only for student registration.");
+        }
         User user = registerUserService.execute(dto);
 
         UserResponseDTO response = new UserResponseDTO(
@@ -33,8 +37,8 @@ public class UserController {
                 user.getUsername(),
                 user.getEmail(),
                 user.getRole(),
-                (user instanceof Student s) ? s.getRegistrationNumber() : null,
-                (user instanceof Teacher t && t.getSubject() != null) ? t.getSubject().getName() : null
+                ((Student) user).getRegistrationNumber(),
+                null
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
