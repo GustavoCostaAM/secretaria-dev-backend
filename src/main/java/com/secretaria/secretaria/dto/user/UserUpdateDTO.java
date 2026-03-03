39217d617739dbba83cc7ac9e23665fc62c24872
Kevin de Oliveira <kevin.oliveira@germinare.org.br>
2026-03-03 15:51:26 -0300

    bugifix: Implement username in update DTO

--- src/main/java/com/secretaria/secretaria/dto/user/UserUpdateDTO.java
+++ src/main/java/com/secretaria/secretaria/dto/user/UserUpdateDTO.java
@@ -1,7 +1,9 @@
 package com.secretaria.secretaria.dto.user;
 
+import com.secretaria.secretaria.model.UserRole;
+
 public record UserUpdateDTO(
         String name,
         String email,
-        Long registrationNumber
+        String username
 ) {}
